f17c6e3e5ecd928fbb2bcb424cb8b5dd03ef6f7e
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-27 09:24:21 -0500

    feat: :sparkles: reemplazamos lineas con fragmenrt y works

--- components/myHeader.js
+++ components/myHeader.js
@@ -54,15 +54,28 @@ export default{
         },
         
     ],
-    listTitle(){
-        document.querySelector("#title").insertAdjacentHTML("beforeend",`<a class="blog-header-logo" >${this.title.name}</a>`)
-    },
-    listarItems(){
-        let plantilla= "";
-        this.pingPongItems.forEach((val, id) => {
-            plantilla += `<a class="head p-2 link-secondary" href="${val.href}">${val.name}</a>`
+    
+    show(){
+        // creamos el worker
+        const ws = new Worker("storage/wsMyHeader.js",{type:"module"});
+
+        //enviamos un mensaje el worker
+        ws.postMessage({module: "listTitle", data : this.title});
+        ws.postMessage({module: "listarItems", data : this.pingPongItems});
+        let id = ["#title","#pingPongItems"];
+        let count = 0;
+        //esto es lo que llega del worker
+        ws.addEventListener("message",(e)=>{
+
+            //estamos parseando lo que trae el evento (mensaje)
+            let doc = new DOMParser().parseFromString(e.data, "text/html");
+            
+            //insertamos en nuestro index, en el selector #pingPongItems
+            document.querySelector(id[count]).append(...doc.body.children);
+
+            //finalizamos el worker
+            (id.length-1==count) ? ws.terminate():count++;
         });
-        document.querySelector("#pingPongItems").insertAdjacentHTML("beforeend",plantilla);
     }
 }
 
