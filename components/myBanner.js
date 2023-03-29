9de67d831a7798193b2a6ad62cd1105d99e80eef
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-28 20:08:53 -0500

    fix: :bug: Arreglo del Banner

--- components/myBanner.js
+++ components/myBanner.js
@@ -1,14 +1,10 @@
+import config from "../storage/config.js";
 export default{
-    image: "../img/two-tennis-rackets-and-ball-against-net-on-table-P37HCQZ-e1602572035813.jpg",
-    comp:{
-        title: "Tenis de mesa",
-        paragraph:`El tenis de mesa (también conocido como ping-pong o pimpón) es un deporte de raqueta que se disputa entre dos jugadores o dos parejas (dobles). Es un deporte olímpico desde Seúl 1988, y el deporte con mayor número de practicantes, con 40 millones de jugadores compitiendo en todo el mundo.1​2​3​ Según un estudio realizado por la NASA, es el deporte más complicado que un ser humano puede practicar a nivel profesional.`,
-        btn:{
-            name:"Saber más.....",
-            href:"https://es.wikipedia.org/wiki/Tenis_de_mesa"
-            },
-    },
+    
     showImage(){
+
+        config.dataMyBanner();
+        Object.assign(this, JSON.parse(localStorage.getItem("myBanner")));
         document.querySelector(".imgStyle").style.backgroundImage = `url(${this.image})`;
     },
     show(){
